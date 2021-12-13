1be7abefef2c079113b008f049f8157ff8a3dde5
Vito9009 <yan.vi193@gmail.com>
2021-12-13 18:42:07 +0100

    aggiunta-riusltati-dadi

--- dadi/js/myscript.js
+++ dadi/js/myscript.js
@@ -11,12 +11,13 @@ console.log(computerNumber);
 // messaggi da mostrare in caso di vittoria / sconfitta o pareggio
 
 if(humanNumber > computerNumber){
-    container.innerHTML = '<div class="box superman">' + "Questo match è stato vinto da superman. Hey computer, hai perso!" + '</div>';
+    container.innerHTML = '<div class="box superman">' + "Questo match è stato vinto da superman. Hey computer, hai perso!" + '<br/>' + "Numero superman:" + " " + humanNumber + '<br/>' + "Numero computer:" + " " + computerNumber + '</div>';
 }else if(humanNumber < computerNumber){
-    container.innerHTML = '<div class="box computer">' + "Il computer ha vinto! Hey superman, vuoi arrenderti?" + '</div>';
+    container.innerHTML = '<div class="box computer">' + "Il computer ha vinto! Hey superman, vuoi arrenderti?" + '<br/>' + "Numero superman:" + " " + humanNumber + '<br/>' + "Numero computer:" + " " + computerNumber + '</div>';
 }else {
-    container.innerHTML = '<div class="box draw">Questa partita termina con un pareggio. Volete sapere chi vincerà? Continuate a giocare!</div>';
+    container.innerHTML = '<div class="box draw">' + "Questa partita termina con un pareggio. Volete sapere chi vincerà? Continuate a giocare!" + '<br/>' + "Numero superman:" + " " + humanNumber + '<br/>' + "Numero computer:" + " " + computerNumber + '</div>';
 }
+
 
 /*
 funzione per restituire un numero compreso tra min e max (inclusi)
