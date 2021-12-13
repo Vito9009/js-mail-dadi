1bef341dd9c439e30a78ed6f75a4e5f923745159
Vito9009 <yan.vi193@gmail.com>
2021-12-13 17:33:27 +0100

    sostituzione-console.log-con-inner-html-in-dadi

--- dadi/js/myscript.js
+++ dadi/js/myscript.js
@@ -1,4 +1,4 @@
-const humanPlayer = document.querySelector("persona");
+const humanPlayer = document.querySelector("superman");
 const aIPlayer = document.querySelector("computer");
 
 const humanNumber = Math.floor(Math.random() * 6 + 1) + Math.floor(Math.random() * 6 + 1);
@@ -6,11 +6,10 @@ const computerNumber = Math.floor(Math.random() * 6 + 1) + Math.floor(Math.rando
 
 
 if(humanNumber > computerNumber){
-    console.log("Questo match è stato vinto dall'uomo.");
-    console.log("Hey computer, hai perso!");
+    container.innerHTML = 
+    '<div class="box superman">' + "Questo match è stato vinto da superman. Hey computer, hai perso!" + '</div>';
 }else if(humanNumber < computerNumber){
-    console.log("Il computer ha vinto!");
-    console.log("Hey umano, ti sembra di essere in un film?");
+    container.innerHTML = '<div class="box computer">' + "Il computer ha vinto! Hey superman, vuoi arrenderti?" + '</div>';
 }else {
-    console.log("Questa partita termina con un pareggio. Volete sapere chi vincerà? Continuate a giocare!");
+    container.innerHTML = '<div class="box draw">Questa partita termina con un pareggio. Volete sapere chi vincerà? Continuate a giocare!</div>';
 }
